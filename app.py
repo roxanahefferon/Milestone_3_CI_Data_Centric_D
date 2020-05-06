ed7b2311e3ab30c0baa77e0070713b79cf86790f
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-06 21:54:50 +0000

    initial commit, app.py, templates routing and viewing ok

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
+@app.route("/add_own")
+def add_own():
+    return render_template("add_own.html")
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
