660dcbd0bcd289252f22415c01935716d6a8683f
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-10 10:48:27 +0000

    storing in dict

--- app.py
+++ app.py
@@ -125,12 +125,9 @@ messages = []
 def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
-
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
@@ -147,7 +144,7 @@ def test():
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
